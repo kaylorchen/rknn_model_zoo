e38aa39fdc1abd920238a14157d097551144f903
kaylorchen <kaylor.chen@qq.com>
2024-01-22 16:06:28 +0800

    format

--- examples/yolov8_kaylor/cpp/postprocess.h
+++ examples/yolov8_kaylor/cpp/postprocess.h
@@ -2,10 +2,12 @@
 #define _RKNN_YOLOV8_DEMO_POSTPROCESS_H_
 
 #include <stdint.h>
+
 #include <vector>
-#include "rknn_api.h"
+
 #include "common.h"
 #include "image_utils.h"
+#include "rknn_api.h"
 
 #define OBJ_NAME_MAX_SIZE 64
 #define OBJ_NUMB_MAX_SIZE 128
@@ -16,21 +18,23 @@
 // class rknn_app_context_t;
 
 typedef struct {
-    image_rect_t box;
-    float prop;
-    int cls_id;
+  image_rect_t box;
+  float prop;
+  int cls_id;
 } object_detect_result;
 
 typedef struct {
-    int id;
-    int count;
-    object_detect_result results[OBJ_NUMB_MAX_SIZE];
+  int id;
+  int count;
+  object_detect_result results[OBJ_NUMB_MAX_SIZE];
 } object_detect_result_list;
 
 int init_post_process();
 void deinit_post_process();
 char *coco_cls_to_name(int cls_id);
-int post_process(rknn_app_context_t *app_ctx, rknn_output *outputs, letterbox_t *letter_box, float conf_threshold, float nms_threshold, object_detect_result_list *od_results);
+int post_process(rknn_app_context_t *app_ctx, rknn_output *outputs,
+                 letterbox_t *letter_box, float conf_threshold,
+                 float nms_threshold, object_detect_result_list *od_results);
 
 void deinitPostProcess();
-#endif //_RKNN_YOLOV8_DEMO_POSTPROCESS_H_
+#endif  //_RKNN_YOLOV8_DEMO_POSTPROCESS_H_
